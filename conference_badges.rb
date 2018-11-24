498947ebea0326d7f47c26e565f8de610248dd0e
Katie Ashlin <akka726@outlook.com>
2018-11-24 18:53:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,7 @@ end
 
 def assign_rooms(speakers)
   new_arr = []
-  speakers.each_with_index { |name, i|
+  speakers.each_with_index{|name, i|
     
   }
   "Hello, #{name}! You'll be assigned to room #{i}!"
