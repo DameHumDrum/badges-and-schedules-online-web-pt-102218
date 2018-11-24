9ab27e69ad35e6eab087410d011a99dd7ad96acd
Katie Ashlin <akka726@outlook.com>
2018-11-24 18:41:49 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,14 +1,9 @@
 def badge_maker(name)
-  puts "Hello my name is #{name}."
+  puts "Hello, my name is #{name}"
 end
 
 def batch_badge_creator(names)
-  names.map{ |name| badge_maker(name) }
-end
-
-def assign_rooms(speakers)
-  new_arr = []
-  speakers.each_with_index{ |speaker, index|
-    new_arr << "Hello, #{speaker}! You'll stay in room #{index+1}." }
-      new_arr
+  names.map do |name|
+    badge_maker(name)
+  end
 end
